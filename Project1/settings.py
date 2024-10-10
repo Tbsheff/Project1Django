0ba7203b8466ef3d30caa96eb0ab57ab2056f849
Tbsheff <92263166+Tbsheff@users.noreply.github.com>
2024-10-10 10:03:23 -0600

    commit

--- Project1/settings.py
+++ Project1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+37yof5uwca@jy%d^ohvd1ctumejlikvrpcu@3z&3rtqs&%n=7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
